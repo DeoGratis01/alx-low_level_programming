f96b24f1c1d54be04e72ca41c3789eceb3a182ef
DeoGratis01 <lamaianslem@gmail.com>
2022-09-10 15:20:13 -0700

    Size C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,3 +16,8 @@ long long long_long_type;
 printf("Size of a char: %ld byte(s)\n", sizeof(char_type));
 printf("Size of an int: %ld byte(s)\n", sizeof(integer_type));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long_type));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long_long_type));
+printf("Size of a float: %ld byte(s)\n", sizeof(float_type));
+return (0);
+}
+
