b0990b6517870da7f07f61ffecba6f9c608194a2
DeoGratis01 <lamaianslem@gmail.com>
2022-09-13 09:38:16 -0700

    Alphabet C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - check the code
+* print_alphabet: 'Print Strings' 
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
