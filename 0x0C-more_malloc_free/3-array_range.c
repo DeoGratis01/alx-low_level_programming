0e4656a5668b7a33d428baa4a82b9ec10a450814
DeoGratis01 <lamaianslem@gmail.com>
2022-10-06 19:43:22 -0700

    Task array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -24,7 +24,7 @@ int *array_range(int min, int max)
 	if (array == NULL)
 		return (NULL);
 
-	fot (index = 0; index < size; index++)
+	for (index = 0; index < size; index++)
 		array[index] = min++;
 
 	return (array);
